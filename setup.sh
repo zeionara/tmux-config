6d57f999ef38406f176e8eba2d5468cc4a3c6c00
Zeio Nara <zeionara@gmail.com>
2023-02-21 20:12:35 +0000

    Added commands for setting up dependencies

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 tmux -V && echo 'Tmux is already installed, proceeding to the configuration...' || echo 'Installing tmux...' && sudo apt-get update && sudo apt-get install tmux
 
+echo 'Installing widget dependencies...'
+sudo apt-get update && sudo apt-get install gawk net-tools coreutils -y
+
 git clone https://github.com/zeionara/tmux-config.git $HOME/tmux-config
 $HOME/tmux-config/install.sh
