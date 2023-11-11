5c33ebec5fd270cf25ebe90c6bf66b1391a8a9d5
Zeio Nara <zeionara@gmail.com>
2023-11-12 01:06:58 +0300

    fix(setup): updated setup script

--- setup.sh
+++ setup.sh
@@ -7,21 +7,34 @@ echop () {
 echo '🏁 Installing tmux. Checking if it is already installed...'
 
 if test -z $(which tmux); then
-    echop 'Installing tmux...'
+  echop 'Installing tmux...'
+
+  if test $(which apt-get 2> /dev/null); then
     sudo apt-get update
     sudo apt-get install tmux -y || true
+
+    echop 'Installing widget dependencies...'
+
+    sudo apt-get install gawk net-tools coreutils -y
+  elif test $(which pacman 2> /dev/null); then
+    sudo pacman -Syu
+    sudo pacman -S tmux
+  elif test $(which emerge 2> /dev/null); then
+    sudo emerge --ask app-misc/tmux
+  else
+    echo "Can't install tmux"
+    exit 1
+  fi
 else
-    echop 'Found existing tmux installation'
+  echop 'Found an existing tmux installation'
 fi
-
-echop 'Installing widget dependencies...'
-
-sudo apt-get update && sudo apt-get install gawk net-tools coreutils -y
 
 echop 'Getting tmux config repo...'
 
-git clone https://github.com/zeionara/tmux-config.git $HOME/tmux-config
+ROOT="$HOME/tmux-config"
+
+git clone https://github.com/zeionara/tmux-config.git "$ROOT"
 
 echop 'Installing tmux config...'
 
-$HOME/tmux-config/install.sh
+"$ROOT/install.sh"
